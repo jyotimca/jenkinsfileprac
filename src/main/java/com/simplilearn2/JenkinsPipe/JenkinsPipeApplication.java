dc47a836807cc29ece1c884bf6163cce7dcd4b3a
student <jyoti.mishras@gmail.com>
2021-02-27 17:09:04 +0000

    added jenkins pipeline

---
+++ src/main/java/com/simplilearn2/JenkinsPipe/JenkinsPipeApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn2.JenkinsPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn2/JenkinsPipe/JenkinsPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn2.JenkinsPipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
